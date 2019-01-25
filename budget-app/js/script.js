3e086d17198d7a779ea8b3195d97f9894e698041
Igore4ik <43449370+Igore4ik@users.noreply.github.com>
2019-01-25 20:43:11 +0200

    This is my homework from JS courses

---
+++ budget-app/js/script.js
@@ -0,0 +1,27 @@
+let start = document.getElementById('start'),
+    budget = document.getElementsByClassName('budget-value'),
+    daybudget = document.getElementsByClassName('daybudget-value') ,
+    level = document.getElementsByClassName('level-value'),
+    expenses = document.getElementsByClassName('expenses-value'),
+    optionalexpenses = document.getElementsByClassName('optionalexpenses-value'),
+    income = document.getElementsByClassName('income-value'),
+    monthsavings = document.getElementsByClassName('monthsavings-value'),
+    yearsavings = document.getElementsByClassName('yearsavings-value'),
+    expensesItem = document.getElementsByClassName('expenses-item'),
+    expensesItemBtn = document.getElementsByTagName('button'),
+    optionalexpensesBtn = document.getElementsByTagName('button'),
+    countBudgetBtn = document.getElementsByTagName('button'),
+    optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income')  ,
+    savingsCheckbox = document.querySelector('#savings')  ,
+    chooseSum = document.querySelector('.choose-sum')  ,
+    choosePercent = document.querySelector('.choose-percent')  ,
+    year = document.querySelector('.year-value'),
+    month = document.querySelector('.month-value'),
+    day = document.querySelector('.day-value');
+
+
+
+    
+
+
